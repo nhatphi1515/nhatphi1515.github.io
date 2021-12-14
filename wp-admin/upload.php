a411eb1f58c04721e2c277648f4da7c1f30a0aa1
nhatphi1515 <78189058+nhatphi1515@users.noreply.github.com>
2021-12-14 16:36:10 +0700

    upload

---
+++ wp-admin/upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://https://github.com/nhatphi1515/nhatphi1515.github.io.git/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://https://github.com/nhatphi1515/nhatphi1515.github.io.git/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="http://https://github.com/nhatphi1515/nhatphi1515.github.io.git/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php&reauth=1">http://https://github.com/nhatphi1515/nhatphi1515.github.io.git/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php&reauth=1</a></p>
+	</body>
+</html>
